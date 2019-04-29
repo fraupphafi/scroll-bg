fc0a9aa0dd516c03ab1071715db750ce2428e67b
Natalia Semenova <fra.upphafi@gmail.com>
2019-04-29 12:28:21 +0200

    First markup

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('приветики');
+let container = document.querySelector('.container');
+let sections = Array.from(document.querySelectorAll('.container section'));
+let firstColor = sections[0].getAttribute('data-color');
+
+container.style.backgroundColor = firstColor;
+
+console.log(sections);
+console.log(firstColor);
